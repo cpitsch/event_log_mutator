08ff74bd848ce0418aa335aec9183cea72eb59f9
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2024-08-29 02:37:56 +0200

    Add test for TOML parsing

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -30,6 +30,73 @@ pub struct MutationChainConfig {
 
 pub fn parse_toml(path: &PathBuf) -> Result<MutationChainConfig, CliError> {
     let contents = read_to_string(path).unwrap();
-    let res: MutationChainConfig = from_str(&contents).expect("Invalid TOML format");
+    let res = parse_toml_string(&contents);
     Ok(res)
+}
+
+pub fn parse_toml_string(content: &str) -> MutationChainConfig {
+    from_str(content).expect("Invalid TOML format")
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use tests::parametrized_pipeline::ParametrizedMutationConfig;
+
+    use super::parametrized_pipeline::MutationValue;
+    use super::*;
+
+    const TOML_CONTENT: &str = "
+input = \"input_log.xes.gz\"
+output = \"output.xes.gz\"
+
+[pipeline]
+[[pipeline.mutations]]
+type = \"ServiceTimeStdShifter\"
+activity = \"a\"
+probability = 0.5
+standard_deviations = 1.0
+seed=42
+
+[[pipeline.mutations]]
+type = \"ActivityRenamer\"
+activity = \"Send Fine\"
+new_label = \"New Activity\"
+probability = 0.5
+    ";
+
+    #[test]
+    fn config_params_parsed_correctly() {
+        let res = parse_toml_string(TOML_CONTENT);
+
+        assert_eq!(res.input, PathBuf::from_str("input_log.xes.gz").unwrap());
+        assert_eq!(
+            res.output,
+            Some(PathBuf::from_str("output.xes.gz").unwrap())
+        );
+
+        assert!(!res.compress_output); // Not provided, default to false
+        assert_eq!(res.seed, None);
+
+        assert_eq!(
+            res.pipeline,
+            ParametrizedPipelineConfig {
+                mutations: vec![
+                    ParametrizedMutationConfig::ServiceTimeStdShifter {
+                        activity: MutationValue::Value(Some("a".to_string())),
+                        standard_deviations: MutationValue::Value(1.0),
+                        probability: MutationValue::Value(0.5),
+                        seed: Some(MutationValue::Value(42))
+                    },
+                    ParametrizedMutationConfig::ActivityRenamer {
+                        activity: MutationValue::Value("Send Fine".to_string()),
+                        new_label: MutationValue::Value("New Activity".to_string()),
+                        probability: MutationValue::Value(0.5),
+                        seed: None
+                    }
+                ]
+            }
+        )
+    }
 }
