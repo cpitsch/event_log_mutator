97631f47d716ab44fca7f5d12fde89a2258b9a8a
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2024-10-23 23:32:13 +0200

    Add Tests for custom serde

--- src/parsing/custom_serde.rs
+++ src/parsing/custom_serde.rs
@@ -48,3 +48,68 @@ where
         ))),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::parsing::mutation_value::MutationValue;
+    use serde::Deserialize;
+
+    #[derive(Deserialize)]
+    pub struct TestStruct {
+        #[serde(deserialize_with = "deserialize_u64_vec_or_range")]
+        pub my_field: Option<MutationValue<u64>>,
+    }
+
+    #[test]
+    fn test_inclusive_range_expr() {
+        let expr = "1..=25";
+        let res: TestStruct = toml::from_str(format!("my_field=\"{}\"", expr).as_str()).unwrap();
+        let expected: Vec<u64> = (1..=25).collect();
+        assert!(res.my_field.is_some_and(|val| val.get_as_vec() == expected));
+    }
+
+    #[test]
+    fn test_non_inclusive_range_expr() {
+        let expr = "1..25";
+        let res: TestStruct = toml::from_str(format!("my_field=\"{}\"", expr).as_str()).unwrap();
+        let expected: Vec<u64> = (1..25).collect();
+        assert!(res.my_field.is_some_and(|val| val.get_as_vec() == expected));
+    }
+
+    #[test]
+    fn test_lb_greater_than_ub() {
+        let expr = "25..1";
+        let res = toml::from_str::<TestStruct>(format!("my_field=\"{}\"", expr).as_str());
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_using_floats() {
+        let expr = "1..25.7";
+        let res = toml::from_str::<TestStruct>(format!("my_field=\"{}\"", expr).as_str());
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_single_value() {
+        let expr = 7;
+        let res: TestStruct = toml::from_str(format!("my_field={}", expr).as_str()).unwrap();
+        assert!(res.my_field.is_some_and(|val| val.inner_value() == expr));
+    }
+
+    #[test]
+    fn test_vec() {
+        let expr = "[2,4,6,8]";
+        let res: TestStruct = toml::from_str(format!("my_field={}", expr).as_str()).unwrap();
+        let expected: Vec<u64> = vec![2, 4, 6, 8];
+        assert!(res.my_field.is_some_and(|val| val.get_as_vec() == expected));
+    }
+
+    #[test]
+    fn non_range_expr_string_errors() {
+        let expr = "hello, world!";
+        let res = toml::from_str::<TestStruct>(format!("my_field=\"{}\"", expr).as_str());
+        assert!(res.is_err());
+    }
+}
