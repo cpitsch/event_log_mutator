dddacd9f24249a8a85af075a366919caf0c77f03
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2025-01-28 16:03:43 +0100

    FollowerFilter: Make activity sets required, no default to all activities

--- src/mutators/filters/follower_filter.rs
+++ src/mutators/filters/follower_filter.rs
@@ -5,7 +5,7 @@ use crate::{
     mutators::DisplayVec,
     parsing::traits::DirName,
     utils::{
-        attributes::{get_activities, get_activity_label, AttributeResult},
+        attributes::{get_activity_label, AttributeResult},
         errors::retain_err,
     },
 };
@@ -13,12 +13,10 @@ use crate::{
 /// Mutation to retain only the cases which contain eventually-follows relations between certain activities
 #[derive(DirName)]
 pub struct FollowerFilter {
-    /// The relation "trigger" activities to filter for. Defaults to all activities (no cases
-    /// filtered).
-    trigger_activities: Option<DisplayVec<String>>,
-    /// The relation "reaction" activities to filter for. Defaults to all activities (no cases
-    /// filtered).
-    reaction_activities: Option<DisplayVec<String>>,
+    /// The relation "trigger" activities to filter for.
+    trigger_activities: DisplayVec<String>,
+    /// The relation "reaction" activities to filter for.
+    reaction_activities: DisplayVec<String>,
     /// The window (number of events) to accept for an eventually-follows relation.
     /// For instance, a range of 1 means we only consider Directly-Follows relations.
     /// Defaults to the trace length
@@ -28,12 +26,12 @@ pub struct FollowerFilter {
 
 impl FollowerFilter {
     pub fn new(
-        trigger_activities: Option<impl Into<DisplayVec<String>>>,
-        reaction_activities: Option<impl Into<DisplayVec<String>>>,
+        trigger_activities: impl Into<DisplayVec<String>>,
+        reaction_activities: impl Into<DisplayVec<String>>,
     ) -> Self {
         FollowerFilter {
-            trigger_activities: trigger_activities.map(|acts| acts.into()),
-            reaction_activities: reaction_activities.map(|acts| acts.into()),
+            trigger_activities: trigger_activities.into(),
+            reaction_activities: reaction_activities.into(),
             range: None,
         }
     }
@@ -43,12 +41,7 @@ impl FollowerFilter {
         self
     }
 
-    fn keep_trace(
-        &self,
-        trace: &Trace,
-        trigger_activities: &[String],
-        reaction_activities: &[String],
-    ) -> AttributeResult<bool> {
+    fn keep_trace(&self, trace: &Trace) -> AttributeResult<bool> {
         let range = self.range.unwrap_or(trace.events.len());
         let trace_activities: Vec<String> = trace
             .events
@@ -58,11 +51,11 @@ impl FollowerFilter {
 
         let is_trigger_act: Vec<bool> = trace_activities
             .iter()
-            .map(|act| trigger_activities.contains(act))
+            .map(|act| self.trigger_activities.contains(act))
             .collect();
         let is_reaction_act: Vec<bool> = trace_activities
             .iter()
-            .map(|act| reaction_activities.contains(act))
+            .map(|act| self.reaction_activities.contains(act))
             .collect();
 
         let result = (0..trace_activities.len() - 1).any(|i| {
@@ -77,24 +70,8 @@ impl FollowerFilter {
 
 impl LogMutator for FollowerFilter {
     fn apply_mut(&mut self, log: &mut EventLog) -> MutationResult<()> {
-        let all_activities: Vec<String> = get_activities(log)
-            .map_err(|e| MutationError::MissingAttributeError("EndpointFilter", e))?
-            .into_iter()
-            .collect();
-
-        let trigger_acts = self
-            .trigger_activities
-            .as_deref()
-            .unwrap_or(all_activities.as_slice());
-        let react_acts = self
-            .reaction_activities
-            .as_deref()
-            .unwrap_or(all_activities.as_slice());
-
-        retain_err(&mut log.traces, |trace| {
-            self.keep_trace(trace, trigger_acts, react_acts)
-        })
-        .map_err(|e| MutationError::MissingAttributeError("EndpointFilter", e))?;
+        retain_err(&mut log.traces, |trace| self.keep_trace(trace))
+            .map_err(|e| MutationError::MissingAttributeError("EndpointFilter", e))?;
         Ok(())
     }
 }
@@ -109,42 +86,31 @@ mod tests {
     #[rstest]
     fn test_keep_trace(abcd_trace: Trace) {
         let filter_1 =
-            FollowerFilter::new(Some(vec!["a".to_string()]), Some(vec!["c".to_string()]))
-                .with_range(1);
+            FollowerFilter::new(vec!["a".to_string()], vec!["c".to_string()]).with_range(1);
 
         // It is not directly (1) following, but eventually (2)
-        assert!(!filter_1
-            .keep_trace(&abcd_trace, &["a".to_string()], &["c".to_string()])
-            .unwrap());
+        assert!(!filter_1.keep_trace(&abcd_trace).unwrap());
 
         let filter_2 =
-            FollowerFilter::new(Some(vec!["a".to_string()]), Some(vec!["c".to_string()]))
-                .with_range(2);
+            FollowerFilter::new(vec!["a".to_string()], vec!["c".to_string()]).with_range(2);
 
         // This should succeed since it is exactly a distance of 2
-        assert!(filter_2
-            .keep_trace(&abcd_trace, &["a".to_string()], &["c".to_string()])
-            .unwrap());
+        assert!(filter_2.keep_trace(&abcd_trace).unwrap());
 
         let filter_3 =
-            FollowerFilter::new(Some(vec!["a".to_string()]), Some(vec!["c".to_string()]))
-                .with_range(3);
+            FollowerFilter::new(vec!["a".to_string()], vec!["c".to_string()]).with_range(3);
 
         // Distance of 2 should still count for larger windows
-        assert!(filter_3
-            .keep_trace(&abcd_trace, &["a".to_string()], &["c".to_string()])
-            .unwrap());
+        assert!(filter_3.keep_trace(&abcd_trace).unwrap());
     }
 
     #[rstest]
     fn test_no_matches_discards(abcd_trace: Trace) {
-        let filter = FollowerFilter::new(Some(vec!["y".to_string()]), Some(vec!["z".to_string()]))
-            .with_range(4); // 4 = Entire trace
+        let filter =
+            FollowerFilter::new(vec!["y".to_string()], vec!["z".to_string()]).with_range(4); // 4 = Entire trace
 
         // Should discard the trace since this relation does not exist in the trace
         // since the activities "y" and "z" do not exist in the trace.
-        assert!(!filter
-            .keep_trace(&abcd_trace, &["y".to_string()], &["z".to_string()])
-            .unwrap());
+        assert!(!filter.keep_trace(&abcd_trace).unwrap());
     }
 }
