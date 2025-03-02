afaaaccc53c3d46d97b490435e2fa8665a5894f5
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2025-03-02 17:36:43 +0100

    Clean up PartialOrderCreator

--- src/mutators/partial_order_creator.rs
+++ src/mutators/partial_order_creator.rs
@@ -3,7 +3,7 @@ use process_mining::event_log::{AttributeValue, Trace};
 use crate::{
     mutation::{MutationError, MutationResult, TraceMutator},
     parsing::traits::DirName,
-    utils::attributes::{get_complete_timestamp, get_start_timestamp, set_start_timestamp},
+    utils::attributes::{get_complete_timestamp, set_start_timestamp},
 };
 
 /// Mutation to add service time information to an event log by assuming the timespan
@@ -22,25 +22,25 @@ impl PartialOrderCreator {
 
 impl TraceMutator for PartialOrderCreator {
     fn apply_mut(&mut self, trace: &mut Trace) -> MutationResult<()> {
-        if let Some(evt) = trace.events.get_mut(0) {
-            // Set its start_timestamp to its completion timestamp since we have no
-            // information on this. This means the first event always has service time 0.
-            let first_complete_timestamp = get_complete_timestamp(evt)
+        let Some(evt) = trace.events.first_mut() else {
+            // No events, so no work to do.
+            return Ok(());
+        };
+
+        // Set its start_timestamp to its completion timestamp since we have no
+        // information on this. This means the first event always has service time 0.
+        let first_complete_timestamp = get_complete_timestamp(evt)
+            .map_err(|e| MutationError::MissingAttributeError("PartialOrderCreator", e))?;
+        set_start_timestamp(evt, AttributeValue::Date(first_complete_timestamp));
+
+        let mut previous_timestamp = first_complete_timestamp;
+
+        for event in trace.events.iter_mut().skip(1) {
+            set_start_timestamp(event, AttributeValue::Date(previous_timestamp));
+            previous_timestamp = get_complete_timestamp(event)
                 .map_err(|e| MutationError::MissingAttributeError("PartialOrderCreator", e))?;
-            set_start_timestamp(evt, AttributeValue::Date(first_complete_timestamp));
-
-            let mut previous_timestamp = first_complete_timestamp;
-
-            for event in trace.events.iter_mut().skip(1) {
-                set_start_timestamp(event, AttributeValue::Date(previous_timestamp));
-                previous_timestamp = get_complete_timestamp(event)
-                    .map_err(|e| MutationError::MissingAttributeError("PartialOrderCreator", e))?;
-            }
         }
-        trace
-            .events
-            .iter()
-            .for_each(|evt| assert!(get_start_timestamp(evt).is_ok()));
+
         Ok(())
     }
 }
