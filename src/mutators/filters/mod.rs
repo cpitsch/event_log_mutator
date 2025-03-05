b0b6fc9fb48fafdee13a830ca80af80d382d17bd
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2025-03-05 14:18:32 +0100

    Add trace length filter. TODO: decide either for case or trace, not both names
    CaseDurationFilter vs TraceLengthFilter

--- src/mutators/filters/mod.rs
+++ src/mutators/filters/mod.rs
@@ -2,10 +2,12 @@ pub mod attribute_value_filter;
 pub mod case_duration_filter;
 pub mod endpoint_filter;
 pub mod follower_filter;
+pub mod trace_length_filter;
 pub mod variant_support_filter;
 
 pub use attribute_value_filter::AttributeFilter;
 pub use case_duration_filter::CaseDurationFilter;
 pub use endpoint_filter::EndpointFilter;
 pub use follower_filter::FollowerFilter;
+pub use trace_length_filter::TraceLengthFilter;
 pub use variant_support_filter::VariantSupportFilter;
---
+++ src/mutators/filters/trace_length_filter.rs
@@ -0,0 +1,94 @@
+use process_mining::{event_log::Trace, EventLog};
+
+use super::case_duration_filter::ComparisonSense;
+use crate::{
+    mutation::{LogMutator, MutationError, MutationResult},
+    parsing::traits::DirName,
+    utils::{attributes::AttributeResult, errors::retain_err},
+};
+
+#[derive(DirName)]
+pub struct TraceLengthFilter {
+    length: usize,
+    sense: ComparisonSense,
+}
+
+impl TraceLengthFilter {
+    pub fn new(length: usize) -> Self {
+        Self {
+            length,
+            sense: ComparisonSense::default(),
+        }
+    }
+
+    pub fn leq(mut self) -> Self {
+        self.sense = ComparisonSense::LEQ;
+        self
+    }
+
+    pub fn geq(mut self) -> Self {
+        self.sense = ComparisonSense::GEQ;
+        self
+    }
+
+    pub fn with_sense(mut self, sense: ComparisonSense) -> Self {
+        self.sense = sense;
+        self
+    }
+
+    fn keep_trace(&self, trace: &Trace) -> AttributeResult<bool> {
+        Ok(self.sense.compare(&trace.events.len(), &self.length))
+    }
+}
+
+impl LogMutator for TraceLengthFilter {
+    fn apply_mut(&mut self, log: &mut EventLog) -> MutationResult<()> {
+        retain_err(&mut log.traces, |trace| self.keep_trace(trace))
+            .map_err(|e| MutationError::MissingAttributeError("TraceLengthFilter", e))?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use process_mining_macros::event_log;
+
+    use super::*;
+
+    #[test]
+    fn leq() {
+        let mut filter = TraceLengthFilter::new(2).leq();
+        let log = event_log!([a], [a, b], [a, b, c], [a, b, c, d]);
+
+        assert_eq!(2, filter.apply(&log).unwrap().traces.len());
+    }
+    #[test]
+    fn geq() {
+        let mut filter = TraceLengthFilter::new(2).geq();
+        let log = event_log!([a], [a, b], [a, b, c], [a, b, c, d]);
+
+        assert_eq!(3, filter.apply(&log).unwrap().traces.len());
+    }
+    #[test]
+    fn less() {
+        let mut filter = TraceLengthFilter::new(2).with_sense(ComparisonSense::Less);
+        let log = event_log!([a], [a, b], [a, b, c], [a, b, c, d]);
+
+        assert_eq!(1, filter.apply(&log).unwrap().traces.len());
+    }
+    #[test]
+    fn greater() {
+        let mut filter = TraceLengthFilter::new(2).with_sense(ComparisonSense::Greater);
+        let log = event_log!([a], [a, b], [a, b, c], [a, b, c, d]);
+
+        assert_eq!(2, filter.apply(&log).unwrap().traces.len());
+    }
+
+    #[test]
+    fn empty_trace() {
+        let log = event_log!([], [a], [a, b]);
+        let mut filter = TraceLengthFilter::new(1).with_sense(ComparisonSense::Less);
+
+        assert_eq!(1, filter.apply(&log).unwrap().traces.len());
+    }
+}
